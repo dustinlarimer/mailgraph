715f34b6c1002f41455efeca5d6b2b79f7a424d7
Dustin Larimer <dustinlarimer@gmail.com>
2012-08-02 14:09:07 -0500

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < Neo4j::Rails::Model
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  property :name, :type => String
+  property :email, :type => String, :index => :exact
+  has_n :friends
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailgraph::Application.config.secret_token = 'aa4de3b49ab76f1d5237feeb21ae1d08ba152acadaea5d1891feed4b2eb4910ad9abd8e284171a5172a2c9e5cd230ba3e941b65d1065c10ede9f73009b3a86ca'
