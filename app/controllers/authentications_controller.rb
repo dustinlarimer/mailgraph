201655a84145d0e1565b033375ed76c520040a90
Dustin Larimer <dustinlarimer@gmail.com>
2012-08-05 22:29:22 -0500

    Cleaning up

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,15 +1,12 @@
+require 'gmail_xoauth'
 class AuthenticationsController < ApplicationController
   def index
     @authentications = current_user.authentications if current_user
-    #Authentication.first.destroy
-    #render :text => current_user.id
-    #render :text => Authentication.find(:first, :conditions => {:provider => 'google', :uid => 'dustinlarimer@gmail.com'}).incoming(:user).to_yaml
-    #render :text => Authentication.find(:first, "provider: google AND uid: dustinlarimer@gmail.com").user_id.to_yaml
   end
   
   def create
     callback = request.env['omniauth.auth']
-    auth = Authentication.find(:first, :conditions => {:provider => callback['provider'], :uid => callback['uid']})    
+    auth = Authentication.find(:first, :conditions => {:provider => callback['provider'], :uid => callback['uid']})
     if auth
       # Sign in with auth.user_id
       user = User.find(auth.user_id)
@@ -34,7 +31,6 @@ class AuthenticationsController < ApplicationController
         flash[:notice] = "Account created and authenticated successfully."
         sign_in_and_redirect(:user, new_user)
       else
-        # Save to session and build Authentication later!
         session[:omniauth] = callback.except('extra')
         redirect_to new_user_registration_url
       end
