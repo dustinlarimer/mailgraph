a665ff02c6a31c4efaa4831cf731cd7cacb51215
Dustin Larimer <dustinlarimer@gmail.com>
2012-09-25 20:03:30 -0500

    Added graph paper CSS effect

--- app/controllers/network/google_controller.rb
+++ app/controllers/network/google_controller.rb
@@ -5,99 +5,65 @@ class Network::GoogleController < ApplicationController
   def refresh
     if current_user && current_user.authentications.count > 0
       @google_auth = current_user.authentications.find(params[:google_id])
+      mailbox = Mailbox.find(:first, :conditions => {:user_id => current_user.id, :email => @google_auth.uid})
+      
+      #count_messages = mailbox.messages.count
+      #if count_messages > 0
+      if mailbox.last_update
+        latest_message_datetime = mailbox.last_update #mailbox.messages[count_messages-1].message_datetime
+        puts "Latest message datetime: #{mailbox.last_update}"
+        #puts "#{mailbox.messages[count_messages-1].from.email}"
+      else
+        latest_message_datetime = Time.now - 1.month
+        puts "First round, starting from #{latest_message_datetime}"
+      end
+      
       gmail = Gmail.connect!(:xoauth, @google_auth.uid, 
         :token           => @google_auth.token,
         :secret          => @google_auth.secret,
         :consumer_key    => 'anonymous',
         :consumer_secret => 'anonymous'
       )
-      
-      mailbox = Mailbox.find(:first, :conditions => {:user_id => current_user.id, :email => @google_auth.uid})
-      count_messages = mailbox.messages.count
-      if count_messages > 0
-        latest_message_datetime = mailbox.messages[count_messages-1].message_datetime
-        puts "Latest message datetime: #{latest_message_datetime}"
-        puts "#{mailbox.messages[count_messages-1].from.email}"
-        gmail.mailbox('[Gmail]/All Mail').find(:after => latest_message_datetime).each do |email|
-          if mailbox.messages.any?{ |message| message.message_id == email.envelope.message_id }
-            puts "Message already logged! (#{mailbox.messages.count} Total)"
-          else
-            from = "#{email.envelope.from[0].mailbox}@#{email.envelope.from[0].host}".downcase
-            from_mailbox = Mailbox.find_or_create_by(:email => from)
-            to_mailboxes = Array.new
-            cc_mailboxes = Array.new
+      gmail.mailbox('[Gmail]/All Mail').find(:after => latest_message_datetime).each do |email|
+        if mailbox.messages.any?{ |message| message.message_id == email.envelope.message_id }
+          puts "Message already logged! (#{mailbox.messages.count} Total)"
+        else
+          from = "#{email.envelope.from[0].mailbox}@#{email.envelope.from[0].host}".downcase
+          from_mailbox = Mailbox.find_or_create_by(:email => from)
+          to_mailboxes = Array.new
+          cc_mailboxes = Array.new
 
-            to = email.envelope.to
-            if to
-              to.each do |person|
-                to_email = "#{person.mailbox}@#{person.host}".downcase
-                to_mailboxes << Mailbox.find_or_create_by(:email => to_email)
-              end
+          to = email.envelope.to
+          if to
+            to.each do |person|
+              to_email = "#{person.mailbox}@#{person.host}".downcase
+              to_mailboxes << Mailbox.find_or_create_by(:email => to_email)
             end
-
-            cc = email.envelope.cc
-            if cc
-              cc.each do |person|
-                cc_email = "#{person.mailbox}@#{person.host}".downcase
-                cc_mailboxes << Mailbox.find_or_create_by(:email => cc_email)
-              end
-            end
-
-            Neo4j::Transaction.run do
-              puts Time.parse(email.envelope.date)
-              new_message = mailbox.messages.build(:message_id => "#{email.envelope.message_id}", :message_datetime => Time.parse(email.envelope.date))
-              new_message.from = from_mailbox
-              new_message.to = to_mailboxes
-              new_message.cc = cc_mailboxes
-              new_message.save
-              puts "From: #{new_message.from.email}; To: #{new_message.to.count}; Cc: #{new_message.cc.count}"
-              puts "Messages pulled from this mailbox: #{mailbox.messages.count}"
-            end
-
-            puts "--------------------"
-            puts ""
           end
-        end
-      else
-        latest_message_datetime = Time.now - 1.month
-        puts "First round, starting from #{latest_message_datetime}"
-        gmail.mailbox('[Gmail]/All Mail').find(:after => latest_message_datetime).each do |email|
-          if mailbox
-            from = "#{email.envelope.from[0].mailbox}@#{email.envelope.from[0].host}".downcase
-            from_mailbox = Mailbox.find_or_create_by(:email => from)
-            to_mailboxes = Array.new
-            cc_mailboxes = Array.new
 
-            to = email.envelope.to
-            if to
-              to.each do |person|
-                to_email = "#{person.mailbox}@#{person.host}".downcase
-                to_mailboxes << Mailbox.find_or_create_by(:email => to_email)
-              end
+          cc = email.envelope.cc
+          if cc
+            cc.each do |person|
+              cc_email = "#{person.mailbox}@#{person.host}".downcase
+              cc_mailboxes << Mailbox.find_or_create_by(:email => cc_email)
             end
-
-            cc = email.envelope.cc
-            if cc
-              cc.each do |person|
-                cc_email = "#{person.mailbox}@#{person.host}".downcase
-                cc_mailboxes << Mailbox.find_or_create_by(:email => cc_email)
-              end
-            end
-
-            Neo4j::Transaction.run do
-              puts Time.parse(email.envelope.date)
-              new_message = mailbox.messages.build(:message_id => "#{email.envelope.message_id}", :message_datetime => Time.parse(email.envelope.date))
-              new_message.from = from_mailbox
-              new_message.to = to_mailboxes
-              new_message.cc = cc_mailboxes
-              new_message.save
-              puts "From: #{new_message.from.email}; To: #{new_message.to.count}; Cc: #{new_message.cc.count}"
-              puts "Messages pulled from this mailbox: #{mailbox.messages.count}"
-            end
-
-            puts "--------------------"
-            puts ""
           end
+
+          Neo4j::Transaction.run do
+            puts Time.parse(email.envelope.date)
+            new_message = mailbox.messages.build(:message_id => "#{email.envelope.message_id}", :message_datetime => Time.parse(email.envelope.date))
+            new_message.from = from_mailbox
+            new_message.to = to_mailboxes
+            new_message.cc = cc_mailboxes
+            new_message.save
+            mailbox.last_update = Time.now
+            mailbox.save
+            puts "From: #{new_message.from.email}; To: #{new_message.to.count}; Cc: #{new_message.cc.count}"
+            puts "Messages pulled from this mailbox: #{mailbox.messages.count}"
+          end
+
+          puts "--------------------"
+          puts ""
         end
       end
       
--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -3,11 +3,9 @@ class Mailbox < Neo4j::Rails::Model
   #has_one :authentication
   has_n(:messages).to(Message)
   
-  attr_accessible :user_id, :email
+  attr_accessible :user_id, :email, :last_update
   
-  property :user_id, :type => Fixnum
-  property :email, :type => String
-  
-  index :user_id
-  index :email
+  property :user_id, :type => Fixnum, :index => :exact
+  property :email, :type => String, :index => :exact
+  property :last_update, :type => DateTime, :index => :exact
 end
